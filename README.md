9b47e6eb0959e0b490bf8bb893b03c863b07a019
Serge Rey <sjsrey@gmail.com>
2022-07-29 17:54:45 -0700

    Auto-update README

--- README.md
+++ README.md
@@ -1,5 +1,70 @@
+
 # Today I Learned
 
-A place to capture the nuggets so that I don't repeat the same search again down the road.
+A place to capture the nuggets so that I don't repeat the same search again.
 
 Inspired by [jbranchaud](https://github.com/jbranchaud/til).
+
+
+12 TILs at last count.
+
+## Categories
+
+ - [docker](#docker)
+
+ - [emacs](#emacs)
+
+ - [git](#git)
+
+ - [obsidian](#obsidian)
+
+ - [python](#python)
+
+ - [vim](#vim)
+
+ - [zotero](#zotero)
+
+ - [zsh](#zsh)
+
+---
+
+
+### docker
+
+- [Connection refused](docker/connection_refused.md)
+
+- [Speeding up Docker on Mac M1 When using mounts](docker/speeding_up_mounts_mac.md)
+
+### emacs
+
+- [Forward sync from Emacs to pdf](emacs/forward-sync.md)
+
+### git
+
+- [Large File Storage in Git](git/large-file-storage.md)
+
+- [Cherry picking a commit from one branch to another branch](git/cherry-picking.md)
+
+### obsidian
+
+- [Updating community plug-ins](obsidian/updating_community_plugins.md)
+
+- [Project notes in obsidian](obsidian/project_notes.md)
+
+### python
+
+- [Turning warnings off for a cell in Jupyter](python/warnings-jupyter.md)
+
+### vim
+
+- [Using Bookmarks in NERDTREE](vim/nerdtreebookmarks.md)
+
+- [Refresh NERDTree to the directory of file in buffer](vim/nerdtreebuffer.md)
+
+### zotero
+
+- [Controlling which fields export in Zotero](zotero/export_fields.md)
+
+### zsh
+
+- [Confirming in a shell script](zsh/automatic_confirmation_in_shell.md)
---
+++ til.py
@@ -0,0 +1,75 @@
+"""
+Today I Learned
+
+
+This script updates the Readme.md as new entries accumulate.
+"""
+
+import os
+from collections import defaultdict
+
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk("."):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+# kick out .git/
+# kick out .html
+# kick out this file
+# kick out README.md
+
+categories = defaultdict(list)
+
+for f in listOfFiles:
+    fs = f.split("/")
+    ft = fs[-1].split(".")[-1]
+    cat = fs[1]
+    if fs[1] == ".git" or cat == 'README.md':
+        continue
+    if cat == 'til.py' or cat == 'README.html':
+        continue
+    if ft == 'html':
+        continue
+    print(cat, fs, ft)
+    categories[cat].append(fs[-1])
+
+cats = list(categories.keys())
+cats.sort()
+
+
+cathead = ["## Categories"]
+lines = [f'\n - [{cat}](#{cat})' for cat in cats]
+cathead.extend(lines)
+cathead.append("\n---\n")
+
+n = 0
+for cat in cats:
+    cathead.append(f'\n### {cat}')
+    tils = categories[cat]
+    for til in tils:
+        pth = f'{cat}/{til}'
+        with open(pth, 'r') as f:
+            contents = f.readlines()
+            title = contents[0]
+            title = title.replace("# ", "")
+            print(title)
+            line = f'\n- [{title.strip()}]({pth})'
+            cathead.append(line)
+        n += 1
+
+
+content = "\n".join(cathead)
+
+head = """
+# Today I Learned
+
+A place to capture the nuggets so that I don't repeat the same search again.
+
+Inspired by [jbranchaud](https://github.com/jbranchaud/til).
+
+"""
+
+head = f'{head}\n{n} TILs at last count.\n'
+
+content = head + "\n" + content
+with open('README.md', 'w') as of:
+    of.write(content)
